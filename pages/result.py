d03bf73d9502783967512493dfc62b22a291a95b
Bobby Hutter <bobbyhutter@Bobbys-MacBook-Pro.local>
2023-10-14 16:01:08 -0400

    defined actions for search.py's load & search actions & result.py's result_link_titles & search_input_value functions

--- pages/result.py
+++ pages/result.py
@@ -1,18 +1,20 @@
 class DuckDuckGoResultPage:
 
     SEARCH_INPUT = (By.ID, "search_form_input")
-    RESULT_LINKS = (By.CLASS, "eVNpHGjtxRBq_gLOfGDr LQNqh2U1kzYxREs65IJu")
+    RESULT_LINKS = (By.CLASS_NAME, "eVNpHGjtxRBq_gLOfGDr LQNqh2U1kzYxREs65IJu")
 
     def __init__(self, browser):
         self.browser = browser
 
     def result_link_titles(self):
-        # TODO
-        return []
+        links = self.browser.find_elements(*self.RESULT_LINKS)
+        titles = [link.text for link in links]
+        return titles
 
     def search_input_value(self):
-        # TODO
-        return ""
+        search_input = self.browser.find_elements(*self.SEARCH_INPUT)
+        value = search_input.get_attribute('value')
+        return value
 
     def title(self):
         # TODO
--- pages/search.py
+++ pages/search.py
@@ -1,15 +1,19 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+URL = 'https://duckduckgo.com/'
+
 class DuckDuckGoSearchPage:
 
-    SEARCH_INPUT = (By.CLASS, 'searchbox_input__bEGm3')
+    SEARCH_INPUT = (By.CLASS_NAME, 'searchbox_input__bEGm3')
 
     def __init__(self, browser):
         self.browser = browser
 
     def load(self):
-        # TODO
-        pass
+        self.browser.get(self.URL)
 
     def search(self, phrase):
-        # TODO
-        pass
+        search_input = self.browser.find_element(*self.SEARCH_INPUT)
+        search_input.send_keys(phrase + Keys.RETURN)
 
